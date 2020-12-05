9abb539d652560e7b1998e804378ea784ebc1f3f
Matus Jenca <matus.jenca@gmail.com>
2020-12-05 14:45:24 +0100

    ja neviem co tam dam

--- py/selfenergy_1d.py
+++ py/selfenergy_1d.py
@@ -1,70 +1,92 @@
-import sys
-sys.path.append(".")
-from integrator import Newton,EPSILON
 import numpy as np
 import matplotlib.pyplot as plt
-from math import pi
+from integrator import Newton,EPSILON
+from math import pi as π 
 '''
-#Ked dostavam runtime warningy
-import warnings
-warnings.filterwarnings("error")
+konstanty
+
+
+Zakladne konstanty
 '''
-
-#funkcie
-#log
-def F1(x,y,a,b):
-    try:    
-        return np.log((x+y+a+b)**2+1)
-
-    except RuntimeWarning:
-        print("Runtime Warning","x:",x,"y:",y,"a:",a,"b:",b)
-        return None
-#arctg
-def F2(x,y,a,b):
-    return (x+y+a+b)*np.arctan(x+y+a+b)
-
-#konstanty
-#hmotnost elektronu
-m0=9.109534e-31
+precision=1e2
 #naboj elektronu
 e=1.60217662e-19
-#tau
-tau0=6.58e-15
+#htrans
+h=1.0545718e-34 
+#hmotnost elektronu
+m=9.109534e-31 
 #fermiho energia
-Efermi=10*e
-#fermiho energia uz nanormovana
-Uf=200
+Ef=10*e
 #fermiho polomer
-kf=1.6e10
-#katof y
-ymax=10
-#permitivita vakua
-e0=8.8e-12
-#reciprocna tieniaca dlzka
-ks=kf
-#planckova konstanta
-hbar=1.0545718e-34
-#konstanta pred integralom pri Eself
-const=(2*pi)/(2*pi)**3 *(e**2*ks/e0) *(1/pi) *(1/2)
-def Y(y):
-    citatel=(hbar**2*y**2*ks**2)/(2*m0)
-    menovatel=(hbar)/(2*tau0)
-    return citatel/menovatel
+kf=1.6*10e10
+#tau0
+τ0=6.58e-15
+#hranica integralu cez q
+qmax=10
+#permitivita
+ε0=8.854187e-12
 
+'''
+Odvodene konstanty
+'''
+#tau
+τ=τ0
+#tienenie
+ks=kf
+#energia epsilon_tau
+ετ=(h)/(2*τ)
+#bezrozmerna fermiho energia
+uf=(Ef)/(ετ)
+#konstanta pred integralom
+CONST=(e**2*ks)/(π**3*ε0)
+
+'''
+Pomocne funkcie
+'''
+#energia epsilon q
+def εq(q):
+    return (h**2*q**2)/(2*m)
+#cast vysledku analytickeho integralu (je to vlastne funkcia bez dosadenia hranic)
+def Fpart(x,y,pm,u):
+    return 0.5*(np.log((x+y+pm-u)**2+1))+(x+y+pm-u)*np.arctan(x+y+pm-u)
+#vysledok analytickeho integralu
 def F(x,y):
     a=2*np.sqrt(x*y)
-    return (1/a)*(F2(x,y,a,-Uf)-F2(x,y,-a,-Uf)-F2(x,y,a,0)+F2(x,y,-a,0)+F1(x,y,a,Uf)-F1(x,y,a,0)-F1(x,y,-a,Uf)+F1(x,y,-a,0))
-    
-   
+    return 1/(a)*((Fpart(x,y,a,uf)-Fpart(x,y,-a,uf))-(Fpart(x,y,a,0)-Fpart(x,y,-a,0)))
+'''
+vypocet self enerie
+'''
+#self energia
 def selfEnergy(w):
-    def integrant(y):
-        return ((y**2)/(1+y**2))*F(w,Y(y))
-    integral=[n for n in Newton(integrant,EPSILON,ymax,int(1e2)).integrate()]
-    return const*integral[-1]-integral[0]
+    def integrant(q):
+        return ((q**2)/(q**2+1))*F(w,(εq(q*ks))/(ετ))
+    prim=[_ for _ in Newton(integrant,EPSILON,qmax,int(precision)).integrate()]
+    return CONST*(prim[-1]-prim[0])
+#testovacia self energia
+def testSelfEnergy(w):
+    k=(np.sqrt(2*m*ετ*w))/(h)
+    C=(e**2)/((2*π)**3)
+    F=(kf**2-k**2+ks**2)/(4*k)
+    LN=np.log(((kf+k)**2+ks**2)/((kf-k)**2+ks**2))
+    ARC1=(np.arctan((kf+k)/(ks)))
+    ARC2=(np.arctan((kf-k)/(ks)))
+    return -C*F*LN-ks*(ARC1+ARC2)+kf
+
+    
+
+'''
+telo programu
+'''
 if __name__=='__main__':
-    W=np.linspace(1,2*Uf,100)
-    Eself=np.array([selfEnergy(w) for w in W])
-    print(Eself)
-    plt.plot(W,Eself)
+    print('τ',τ)
+    print('ks',ks)
+    print('ετ',ετ)
+    print('uf',uf)
+    print('CONST',CONST)
+    W=np.linspace(10,2*uf,100)
+    Σ=np.array([selfEnergy(w) for w in W])
+    Σtest=np.array([testSelfEnergy(w) for w in W])
+    plt.plot(W,Σ)
+    #plt.plot(W,Σtest)
     plt.show()
 
