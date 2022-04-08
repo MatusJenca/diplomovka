6b5d93b73c72a60474ebd67ba7807db954f0ff5d
MatusJenca <matus.jenca@gmail.com>
2022-04-08 11:56:25 +0200

    pridal som bakalarku

--- py/newTest.py
+++ py/newTest.py
@@ -1,25 +1,25 @@
 import sys
+
 sys.path.append('./tools')
 import numpy as np
 import matplotlib.pyplot as plt
-from integrator import Newton,EPSILON
+from integrator import Newton, EPSILON
 from selfenergy import SelfEnergy
 from math import pi as π
 
-seFunc=SelfEnergy(qmax=10)
-if __name__=='__main__':
-
-    Erg=np.linspace(0.01,1.5,100)
-    Σ=np.array([seFunc(ε) for ε in Erg])
-    Σ100=np.array([seFunc(ε,taucoef=100) for ε in Erg])
-    test=np.array([seFunc.test(eps) for eps in Erg])
+seFunc = SelfEnergy(qmax=10)
+if __name__ == '__main__':
+    Erg = np.linspace(0.01, 1.5, 100)
+    Σ = np.array([seFunc(ε) for ε in Erg])
+    Σ100 = np.array([seFunc(ε, taucoef=100) for ε in Erg])
+    test = np.array([seFunc.test(eps) for eps in Erg])
     plt.xlabel(r"$\frac{ε}{E_{Fermi}}$[ 1 ]")
     plt.ylabel(r"$Σ_{self}$ [ J ]")
-    plt.xlim(0,1.5)
-    print(Σ100/test)
-    plt.plot(Erg,Σ,linewidth=1,label="τ=τ0")
-    plt.plot(Erg,Σ100,linewidth=1,label="τ=$\infty$")
-    plt.plot(Erg,test,linewidth=1,label="test")
-    plt.legend() 
-    #plt.show()
-    plt.savefig('../img/self_energy_comp')
+    plt.xlim(0, 1.5)
+    print(Σ100 / test)
+    plt.plot(Erg, Σ, linewidth=1, label="τ=τ0")
+    plt.plot(Erg, Σ100, linewidth=1, label="τ=$\infty$")
+    plt.plot(Erg, test, linewidth=1, label="test")
+    plt.legend()
+    plt.show()
+    # plt.savefig('../img/self_energy_comp')
