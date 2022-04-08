0b9dff6a020baec2cfb90bf7f5a1c8507fef2701
MatusJenca <matus.jenca@gmail.com>
2022-04-08 12:17:47 +0200

    mne uz jebe asi.
    Aj tak to refactornem, lebo grecke pismena v nazvoch premennych su na picu. Vzdy boli napicu, rovnako ako Oceania bola vzdy vo vojne s Eastaziou.

---
+++ py/tools/physfunc.py
@@ -0,0 +1,29 @@
+import numpy as np
+from math import pi as pi
+
+
+class PhysFunction:
+    def __init__(self):
+        self.precision = int(1e3)
+        # naboj elektronu
+        self.e = 1.60217662e-19
+        # htrans
+        self.h = 1.0545718e-34
+        # hmotnost elektronu
+        self.m = 9.109534e-31
+        # fermiho polomer
+        self.kf = 1.6e10
+        # tienenie
+        self.ks = self.kf
+        # fermiho energia
+        self.Ef = (2 * self.h ** 2 * self.kf ** 2) / (2 * self.m)
+        print(self.Ef)
+        # fermiho rychlost
+        self.vf = np.sqrt((2 * self.Ef) / (self.m))
+        # hustotastavov na fermiho energii
+        self.rhof = ((self.m / self.h ** 2) ** (3 / 2) * self.Ef ** (1 / 2)) / (2 * pi ** 2)
+        # permitivita
+        self.permitivity = 8.854187e-12
+
+    def __call__(self, x):
+        print(f"Empty physics function (input ={x})")
